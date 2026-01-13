14eabefa78b1b13732a0b0333c4b9f9247b95666
Eric Dahl <eric@skeyelab.com>
2026-01-13 00:51:49 -0500

    feat: match update ticket UI exactly to upstream n8n Zendesk node
    - Restructured update ticket operation to use 'Update Fields' collection
    - Added JSON Parameters toggle for both create and update operations
    - Updated helpers to read from updateFields/additionalFields collections
    - Changed field types to match upstream (multiOptions for tags, options with loadOptionsMethod for groups)
    - Updated all tests to use new nested structure
    - Added eslint-disable comments to match upstream empty string defaults

--- nodes/BetterZendesk/resources/ticket/index.ts
+++ nodes/BetterZendesk/resources/ticket/index.ts
@@ -35,6 +35,21 @@ export const ticketDescription: INodeProperties[] = [
 				},
 			},
 			{
+				name: 'Delete',
+				value: 'delete',
+				action: 'Delete a ticket',
+				description: 'Delete a ticket',
+				routing: {
+					request: {
+						method: 'DELETE',
+						url: '=/tickets/{{$parameter.id}}.json',
+					},
+					output: {
+						postReceive: [zendeskPostReceive],
+					},
+				},
+			},
+			{
 				name: 'Get',
 				value: 'get',
 				action: 'Get a ticket',
@@ -65,6 +80,21 @@ export const ticketDescription: INodeProperties[] = [
 				},
 			},
 			{
+				name: 'Recover',
+				value: 'recover',
+				action: 'Recover a ticket',
+				description: 'Recover a suspended ticket',
+				routing: {
+					request: {
+						method: 'PUT',
+						url: '=/suspended_tickets/{{$parameter.id}}/recover.json',
+					},
+					output: {
+						postReceive: [zendeskPostReceive],
+					},
+				},
+			},
+			{
 				name: 'Update',
 				value: 'update',
 				action: 'Update a ticket',
@@ -82,39 +112,186 @@ export const ticketDescription: INodeProperties[] = [
 					},
 				},
 			},
-			{
-				name: 'Delete',
-				value: 'delete',
-				action: 'Delete a ticket',
-				description: 'Delete a ticket',
-				routing: {
-					request: {
-						method: 'DELETE',
-						url: '=/tickets/{{$parameter.id}}.json',
-					},
-					output: {
-						postReceive: [zendeskPostReceive],
-					},
-				},
-			},
-			{
-				name: 'Recover',
-				value: 'recover',
-				action: 'Recover a ticket',
-				description: 'Recover a suspended ticket',
-				routing: {
-					request: {
-						method: 'PUT',
-						url: '=/suspended_tickets/{{$parameter.id}}/recover.json',
-					},
-					output: {
-						postReceive: [zendeskPostReceive],
-					},
-				},
-			},
 		],
 		default: 'create',
 	},
+
+	/* -------------------------------------------------------------------------- */
+	/*                                ticket:create                               */
+	/* -------------------------------------------------------------------------- */
+	{
+		displayName: 'Description',
+		name: 'description',
+		type: 'string',
+		default: '',
+		displayOptions: {
+			show: {
+				resource: ['ticket'],
+				operation: ['create'],
+			},
+		},
+		required: true,
+		description: 'The first comment on the ticket',
+	},
+	{
+		displayName: 'JSON Parameters',
+		name: 'jsonParameters',
+		type: 'boolean',
+		default: false,
+		displayOptions: {
+			show: {
+				resource: ['ticket'],
+				operation: ['create'],
+			},
+		},
+	},
+	{
+		displayName: 'Additional Fields',
+		name: 'additionalFields',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
+		displayOptions: {
+			show: {
+				resource: ['ticket'],
+				operation: ['create'],
+				jsonParameters: [false],
+			},
+		},
+		options: [
+			{
+				displayName: 'Custom Fields',
+				name: 'customFieldsUi',
+				placeholder: 'Add Custom Field',
+				type: 'fixedCollection',
+				typeOptions: {
+					multipleValues: true,
+				},
+				default: {},
+				options: [
+					{
+						displayName: 'Custom Field',
+						name: 'customFieldsValues',
+						values: [
+							{
+								displayName: 'Name or ID',
+								name: 'id',
+								type: 'options',
+								typeOptions: {
+									loadOptionsMethod: 'getCustomFields',
+								},
+								default: '',
+								description:
+									'Custom field ID. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code/expressions/">expression</a>.',
+							},
+							{
+								displayName: 'Value',
+								name: 'value',
+								type: 'string',
+								default: '',
+								description: 'Custom field Value',
+							},
+						],
+					},
+				],
+			},
+			{
+				displayName: 'External ID',
+				name: 'externalId',
+				type: 'string',
+				default: '',
+				description: 'An ID you can use to link Zendesk Support tickets to local records',
+			},
+			{
+				displayName: 'Group Name or ID',
+				name: 'group',
+				type: 'options',
+				typeOptions: {
+					loadOptionsMethod: 'getGroups',
+				},
+				default: '',
+				description:
+					'The group this ticket is assigned to. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code/expressions/">expression</a>.',
+			},
+			{
+				displayName: 'Recipient',
+				name: 'recipient',
+				type: 'string',
+				default: '',
+				description: 'The original recipient e-mail address of the ticket',
+			},
+			{
+				displayName: 'Status',
+				name: 'status',
+				type: 'options',
+				options: [
+					{ name: 'Closed', value: 'closed' },
+					{ name: 'New', value: 'new' },
+					{ name: 'On-Hold', value: 'hold' },
+					{ name: 'Open', value: 'open' },
+					{ name: 'Pending', value: 'pending' },
+					{ name: 'Solved', value: 'solved' },
+				],
+				// eslint-disable-next-line n8n-nodes-base/node-param-default-wrong-for-options
+				default: '',
+				description: 'The state of the ticket',
+			},
+			{
+				displayName: 'Subject',
+				name: 'subject',
+				type: 'string',
+				default: '',
+				description: 'The value of the subject field for this ticket',
+			},
+			{
+				displayName: 'Tag Names or IDs',
+				name: 'tags',
+				type: 'multiOptions',
+				typeOptions: {
+					loadOptionsMethod: 'getTags',
+				},
+				default: [],
+				description:
+					'The array of tags applied to this ticket. Choose from the list, or specify IDs using an <a href="https://docs.n8n.io/code/expressions/">expression</a>.',
+			},
+			{
+				displayName: 'Type',
+				name: 'type',
+				type: 'options',
+				options: [
+					{ name: 'Question', value: 'question' },
+					{ name: 'Incident', value: 'incident' },
+					{ name: 'Problem', value: 'problem' },
+					{ name: 'Task', value: 'task' },
+				],
+				// eslint-disable-next-line n8n-nodes-base/node-param-default-wrong-for-options
+				default: '',
+				description: 'The type of this ticket',
+			},
+		],
+	},
+	{
+		displayName: 'Additional Fields',
+		name: 'additionalFieldsJson',
+		type: 'json',
+		typeOptions: {
+			alwaysOpenEditWindow: true,
+		},
+		default: '',
+		displayOptions: {
+			show: {
+				resource: ['ticket'],
+				operation: ['create'],
+				jsonParameters: [true],
+			},
+		},
+		description:
+			'Object of values to set as described <a href="https://developer.zendesk.com/rest_api/docs/support/tickets">here</a>',
+	},
+
+	/* -------------------------------------------------------------------------- */
+	/*                                ticket:update                               */
+	/* -------------------------------------------------------------------------- */
 	{
 		displayName: 'Ticket ID',
 		name: 'id',
@@ -124,247 +301,207 @@ export const ticketDescription: INodeProperties[] = [
 		displayOptions: {
 			show: {
 				resource: ['ticket'],
-				operation: ['get', 'update', 'delete', 'recover'],
+				operation: ['update'],
 			},
 		},
 	},
 	{
-		displayName: 'Subject',
-		name: 'subject',
-		type: 'string',
-		default: '',
-		displayOptions: {
-			show: {
-				resource: ['ticket'],
-				operation: ['create', 'update'],
-			},
-		},
-		description: 'The subject of the ticket',
-	},
-	{
-		displayName: 'Description',
-		name: 'description',
-		type: 'string',
-		required: true,
-		default: '',
-		displayOptions: {
-			show: {
-				resource: ['ticket'],
-				operation: ['create'],
-			},
-		},
-		description: 'First public comment on the ticket',
-	},
-	{
-		displayName: 'Public Reply',
-		name: 'publicReply',
-		type: 'string',
-		default: '',
+		displayName: 'JSON Parameters',
+		name: 'jsonParameters',
+		type: 'boolean',
+		default: false,
 		displayOptions: {
 			show: {
 				resource: ['ticket'],
 				operation: ['update'],
 			},
 		},
-		description: 'Adds a public reply as the latest comment',
 	},
 	{
-		displayName: 'Internal Note',
-		name: 'internalNote',
-		type: 'string',
-		default: '',
+		displayName: 'Update Fields',
+		name: 'updateFields',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
 		displayOptions: {
 			show: {
 				resource: ['ticket'],
 				operation: ['update'],
+				jsonParameters: [false],
 			},
 		},
-		description: 'Adds an internal note as the latest comment',
-	},
-	{
-		displayName: 'Assignee Email',
-		name: 'assigneeEmail',
-		type: 'string',
-		default: '',
-		displayOptions: {
-			show: {
-				resource: ['ticket'],
-				operation: ['update'],
-			},
-		},
-		description: 'The e-mail address of the assignee',
-		placeholder: 'agent@example.com',
-	},
-	{
-		displayName: 'External ID',
-		name: 'externalId',
-		type: 'string',
-		default: '',
-		displayOptions: {
-			show: {
-				resource: ['ticket'],
-				operation: ['update'],
-			},
-		},
-		description: 'An ID you can use to link Zendesk Support tickets to local records',
-	},
-	{
-		displayName: 'Group Name or ID',
-		name: 'group',
-		type: 'string',
-		default: '',
-		displayOptions: {
-			show: {
-				resource: ['ticket'],
-				operation: ['update'],
-			},
-		},
-		description:
-			'The group this ticket is assigned to. Specify a group ID (numeric) or name using an <a href="https://docs.n8n.io/code/expressions/">expression</a>.',
-		placeholder: '12345',
-	},
-	{
-		displayName: 'Type',
-		name: 'type',
-		type: 'options',
 		options: [
-			{ name: 'Question', value: 'question' },
-			{ name: 'Incident', value: 'incident' },
-			{ name: 'Problem', value: 'problem' },
-			{ name: 'Task', value: 'task' },
+			{
+				displayName: 'Assignee Email',
+				name: 'assigneeEmail',
+				type: 'string',
+				default: '',
+				description: 'The e-mail address of the assignee',
+			},
+			{
+				displayName: 'Custom Fields',
+				name: 'customFieldsUi',
+				placeholder: 'Add Custom Field',
+				type: 'fixedCollection',
+				typeOptions: {
+					multipleValues: true,
+				},
+				default: {},
+				options: [
+					{
+						displayName: 'Custom Field',
+						name: 'customFieldsValues',
+						values: [
+							{
+								displayName: 'Name or ID',
+								name: 'id',
+								type: 'options',
+								typeOptions: {
+									loadOptionsMethod: 'getCustomFields',
+								},
+								default: '',
+								description:
+									'Custom field ID. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code/expressions/">expression</a>.',
+							},
+							{
+								displayName: 'Value',
+								name: 'value',
+								type: 'string',
+								default: '',
+								description: 'Custom field Value',
+							},
+						],
+					},
+				],
+			},
+			{
+				displayName: 'External ID',
+				name: 'externalId',
+				type: 'string',
+				default: '',
+				description: 'An ID you can use to link Zendesk Support tickets to local records',
+			},
+			{
+				displayName: 'Group Name or ID',
+				name: 'group',
+				type: 'options',
+				typeOptions: {
+					loadOptionsMethod: 'getGroups',
+				},
+				default: '',
+				description:
+					'The group this ticket is assigned to. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code/expressions/">expression</a>.',
+			},
+			{
+				displayName: 'Internal Note',
+				name: 'internalNote',
+				type: 'string',
+				default: '',
+				description: 'Internal Ticket Note (Accepts HTML)',
+			},
+			{
+				displayName: 'Public Reply',
+				name: 'publicReply',
+				type: 'string',
+				default: '',
+				description: 'Public ticket reply',
+			},
+			{
+				displayName: 'Recipient',
+				name: 'recipient',
+				type: 'string',
+				default: '',
+				description: 'The original recipient e-mail address of the ticket',
+			},
+			{
+				displayName: 'Status',
+				name: 'status',
+				type: 'options',
+				options: [
+					{ name: 'Closed', value: 'closed' },
+					{ name: 'New', value: 'new' },
+					{ name: 'On-Hold', value: 'hold' },
+					{ name: 'Open', value: 'open' },
+					{ name: 'Pending', value: 'pending' },
+					{ name: 'Solved', value: 'solved' },
+				],
+				// eslint-disable-next-line n8n-nodes-base/node-param-default-wrong-for-options
+				default: '',
+				description: 'The state of the ticket',
+			},
+			{
+				displayName: 'Subject',
+				name: 'subject',
+				type: 'string',
+				default: '',
+				description: 'The value of the subject field for this ticket',
+			},
+			{
+				displayName: 'Tag Names or IDs',
+				name: 'tags',
+				type: 'multiOptions',
+				typeOptions: {
+					loadOptionsMethod: 'getTags',
+				},
+				default: [],
+				description:
+					'The array of tags applied to this ticket. Choose from the list, or specify IDs using an <a href="https://docs.n8n.io/code/expressions/">expression</a>.',
+			},
+			{
+				displayName: 'Type',
+				name: 'type',
+				type: 'options',
+				options: [
+					{ name: 'Question', value: 'question' },
+					{ name: 'Incident', value: 'incident' },
+					{ name: 'Problem', value: 'problem' },
+					{ name: 'Task', value: 'task' },
+				],
+				// eslint-disable-next-line n8n-nodes-base/node-param-default-wrong-for-options
+				default: '',
+				description: 'The type of this ticket',
+			},
 		],
-		default: 'question',
-		displayOptions: {
-			show: {
-				resource: ['ticket'],
-				operation: ['update'],
-			},
-		},
-		description: 'The type of this ticket',
 	},
 	{
-		displayName: 'Tags',
-		name: 'tags',
-		type: 'string',
-		default: '',
-		displayOptions: {
-			show: {
-				resource: ['ticket'],
-				operation: ['update'],
-			},
-		},
-		description: 'Comma-separated tags to set on the ticket',
-	},
-	{
-		displayName: 'Status',
-		name: 'status',
-		type: 'options',
-		options: [
-			{ name: 'Closed', value: 'closed' },
-			{ name: 'New', value: 'new' },
-			{ name: 'On-Hold', value: 'hold' },
-			{ name: 'Open', value: 'open' },
-			{ name: 'Pending', value: 'pending' },
-			{ name: 'Solved', value: 'solved' },
-		],
-		default: 'new',
-		displayOptions: {
-			show: {
-				resource: ['ticket'],
-				operation: ['create', 'update'],
-			},
-		},
-		description: 'State of the ticket',
-	},
-	{
-		displayName: 'Type',
-		name: 'type',
-		type: 'options',
-		options: [
-			{ name: 'Question', value: 'question' },
-			{ name: 'Incident', value: 'incident' },
-			{ name: 'Problem', value: 'problem' },
-			{ name: 'Task', value: 'task' },
-		],
-		default: 'question',
-		displayOptions: {
-			show: {
-				resource: ['ticket'],
-				operation: ['create'],
-			},
-		},
-		description: 'Type of ticket',
-	},
-	{
-		displayName: 'Tags',
-		name: 'tags',
-		type: 'string',
-		default: '',
-		displayOptions: {
-			show: {
-				resource: ['ticket'],
-				operation: ['create'],
-			},
-		},
-		description: 'Comma-separated tags to set on the ticket',
-	},
-	{
-		displayName: 'Recipient',
-		name: 'recipient',
-		type: 'string',
-		default: '',
-		displayOptions: {
-			show: {
-				resource: ['ticket'],
-				operation: ['create'],
-			},
-		},
-		description: 'Original recipient email address of the ticket',
-	},
-	{
-		displayName: 'Custom Fields (JSON)',
-		name: 'customFields',
+		displayName: 'Update Fields',
+		name: 'updateFieldsJson',
 		type: 'json',
-		default: '',
 		typeOptions: {
 			alwaysOpenEditWindow: true,
 		},
-		displayOptions: {
-			show: {
-				resource: ['ticket'],
-				operation: ['create'],
-			},
-		},
-		description: 'Array of custom field IDs and values (use your Zendesk custom field IDs)',
-	},
-	{
-		displayName: 'External ID',
-		name: 'externalId',
-		type: 'string',
 		default: '',
 		displayOptions: {
 			show: {
 				resource: ['ticket'],
-				operation: ['create'],
-			},
-		},
-		description: 'An ID you can use to link Zendesk Support tickets to local records',
-	},
-	{
-		displayName: 'Group Name or ID',
-		name: 'group',
-		type: 'string',
-		default: '',
-		displayOptions: {
-			show: {
-				resource: ['ticket'],
-				operation: ['create'],
+				operation: ['update'],
+				jsonParameters: [true],
 			},
 		},
 		description:
-			'The group this ticket is assigned to. Specify a group ID (numeric) or name using an <a href="https://docs.n8n.io/code/expressions/">expression</a>.',
-		placeholder: '12345',
+			'Object of values to update as described <a href="https://developer.zendesk.com/rest_api/docs/support/tickets">here</a>',
 	},
+
+	/* -------------------------------------------------------------------------- */
+	/*                                ticket:get                                  */
+	/* -------------------------------------------------------------------------- */
+	{
+		displayName: 'Ticket ID',
+		name: 'id',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				resource: ['ticket'],
+				operation: ['get'],
+			},
+		},
+	},
+
+	/* -------------------------------------------------------------------------- */
+	/*                                ticket:getAll                               */
+	/* -------------------------------------------------------------------------- */
 	{
 		displayName: 'Return All',
 		name: 'returnAll',
@@ -403,6 +540,40 @@ export const ticketDescription: INodeProperties[] = [
 		routing: {
 			output: {
 				maxResults: '={{$value}}',
+			},
+		},
+	},
+
+	/* -------------------------------------------------------------------------- */
+	/*                                ticket:delete                               */
+	/* -------------------------------------------------------------------------- */
+	{
+		displayName: 'Ticket ID',
+		name: 'id',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				resource: ['ticket'],
+				operation: ['delete'],
+			},
+		},
+	},
+
+	/* -------------------------------------------------------------------------- */
+	/*                                ticket:recover                              */
+	/* -------------------------------------------------------------------------- */
+	{
+		displayName: 'Suspended Ticket ID',
+		name: 'id',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				resource: ['ticket'],
+				operation: ['recover'],
 			},
 		},
 	},
